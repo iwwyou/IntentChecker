0b95afce97f6c5ae3616f31b741a628ebbd4d264
inseong <iwwyou@korea.ac.kr>
2024-10-10 18:19:39 +0900

    test

---
+++ test.py
@@ -0,0 +1,120 @@
+import json
+from Analyzer.EnhancedSolidityVisitor import EnhancedSolidityVisitor
+from Analyzer.ContractAnalyzer import ContractAnalyzer
+from antlr4 import *
+from Parser.SolidityLexer import SolidityLexer
+from Parser.SolidityParser import SolidityParser
+
+contract_analyzer = ContractAnalyzer()
+
+def map_context_type(context_type):
+    context_mapping = {
+        'contract': 'interactiveSourceUnit',
+        'library': 'interactiveSourceUnit',
+        'interface': 'interactiveSourceUnit',
+        'enum': 'interactiveSourceUnit',
+        'struct': 'interactiveSourceUnit',
+        'function': 'interactiveSourceUnit',
+        'constructor': 'interactiveSourceUnit',
+        'fallback': 'interactiveSourceUnit',
+        'receive': 'interactiveSourceUnit',
+        'event': 'interactiveSourceUnit',
+        'error': 'interactiveSourceUnit',
+        'modifier': 'interactiveSourceUnit',
+        'stateVariableDeclaration': 'interactiveSourceUnit',
+        'enumMember': 'interactiveEnumUnit',
+        'structMember': 'interactiveStructUnit',
+        'simpleStatement': 'interactiveBlockUnit',
+        'if': 'interactiveBlockUnit',
+        'for': 'interactiveBlockUnit',
+        'while': 'interactiveBlockUnit',
+        'do': 'interactiveBlockUnit',
+        'try': 'interactiveBlockUnit',
+        'return': 'interactiveBlockUnit',
+        'break': 'interactiveBlockUnit',
+        'continue': 'interactiveBlockUnit',
+        'emit': 'interactiveBlockUnit',
+        'doWhileWhile': 'interactiveDoWhileUnit',
+        'catch': 'interactiveCatchClauseUnit',
+        'else_if': 'interactiveIfElseUnit',
+        'else': 'interactiveIfElseUnit'
+    }
+
+    try:
+        return context_mapping[context_type]
+    except KeyError:
+        print(f"Warning: No mapping found for context_type '{context_type}'. Returning None.")
+        return None
+
+def generate_parse_tree(input_stream, context_type):
+    input_stream = InputStream(input_stream)
+    lexer = SolidityLexer(input_stream)
+    token_stream = CommonTokenStream(lexer)
+    parser = SolidityParser(token_stream)
+
+    context_rule = map_context_type(context_type)
+
+    if context_rule == 'interactiveStructUnit':
+        tree = parser.interactiveStructUnit()
+    elif context_rule == 'interactiveEnumUnit':
+        tree = parser.interactiveEnumUnit()
+    elif context_rule == 'interactiveBlockUnit':
+        tree = parser.interactiveBlockUnit()
+    elif context_rule == 'interactiveDoWhileUnit':
+        tree = parser.interactiveDoWhileUnit()
+    elif context_rule == 'interactiveIfElseUnit':
+        tree = parser.interactiveIfElseUnit()
+    elif context_rule == 'interactiveCatchClauseUnit':
+        tree = parser.interactiveCatchClauseUnit()
+    else:
+        tree = parser.interactiveSourceUnit()
+
+    return tree
+
+def simulate_input(test_inputs):
+    for input_data in test_inputs:
+        code = input_data['code']
+        start_line = input_data['startLine']
+        end_line = input_data['endLine']
+
+        contract_analyzer.update_code(start_line, end_line, code)
+        context_type = contract_analyzer.get_current_context_type()
+
+        # Parse the received code based on context_type
+        tree = generate_parse_tree(code, context_type)
+
+        visitor = EnhancedSolidityVisitor(contract_analyzer)
+        visitor.visit(tree)
+
+        # Get and print the analysis result
+        result = contract_analyzer.get_analysis_result()
+        print(json.dumps(result, indent=4))
+
+# Test inputs (replicating multiple steps)
+test_inputs = [
+
+    {
+        'code': 'contract TestContract { \n }',
+        'startLine': 1,
+        'endLine': 2
+    },
+
+    {
+        'code': 'uint256 public a;',  # 다음 줄에 코드 추가
+        'startLine': 2,
+        'endLine': 2
+    },
+    {
+        'code': '\n',  # 다음 줄에 코드 추가
+        'startLine': 3,
+        'endLine': 3
+    },
+    {
+        'code': 'function setA(uint256 _a) public { \n }',  # 함수 추가
+        'startLine': 4,
+        'endLine': 5
+    }
+]
+
+# Simulate input as if coming from VSCode with block structure assumptions
+simulate_input(test_inputs)
